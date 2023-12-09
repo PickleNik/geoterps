69d02db54e96d5795a06436de6ccda6cc16bd970
figsoda <figsoda@pm.me>
2023-12-09 18:16:17 -0500

    boilerplate

---
+++ index.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import express from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const {
+  MONGO_COLLECTION: collection,
+  MONGO_DB_NAME: db,
+  MONGO_DB_PASSWORD: password,
+  MONGO_DB_USERNAME: username,
+} = process.env;
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.rvktjyh.mongodb.net/?retryWrites=true&w=majority`;
+// const cl = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+// cl.connect();
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "pug");
+
+app.get("/", (_, res) => res.render("index"))
+
+app.listen(8000);
+console.log(`Web server started and running at http://localhost:8000`);
